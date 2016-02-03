b8560dd4668ef2b238da74ac50cf18c6143b1b44
grantmk <noreply@learnappdevelopment.com>
2016-02-03 09:37:18 +0000

    Initial Commit

---
+++ pennyplay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pennyplay
+//
+//  Created by grant on 03/02/2016.
+//  Copyright © 2016 GK Micro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
